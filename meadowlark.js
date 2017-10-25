ffc4a9675670cee101cb0f3e1e46531637adbb6a
pchlebda <piotrekchlebda@gmail.com>
2017-10-25 22:38:52 +0200

    Moved 'fortune cookie' functionality into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -22,21 +23,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 app.use(function (req, res, next) {
     res.status(404);
     res.render('404');
 });
-
-
-var fortunes = ["Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",];
 
 
 app.listen(app.get('port'), function () {
