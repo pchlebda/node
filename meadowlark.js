c1a5df273661648121f7509aeccee22aa964e863
Piotr Chlebda <piotr.chlebda@first-utility.com>
2017-10-28 15:22:38 +0200

    Add tests

--- meadowlark.js
+++ meadowlark.js
@@ -18,13 +18,33 @@ app.use(function (err, req, res, next) {
     res.render('500');
 });
 
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', { fortune: fortune.getFortune() });
+    res.render('about', { fortune: fortune.getFortune(), pageTestScript: '/qa/tests-about.js' });
 });
+
+
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function (req, res) {
+    res.render('tours/oregon-coast');
+});
+
+
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
+});
+
 
 app.use(function (req, res, next) {
     res.status(404);
